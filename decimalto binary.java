b86b5229170d6f759806524ad523d4575fdc7b3a
phanindra09898 <130451254+phanindra09898@users.noreply.github.com>
2023-08-30 14:31:46 +0530

    Create decimalto binary.java

---
+++ decimalto binary.java
@@ -0,0 +1,15 @@
+import java.io.*;
+ 
+class binaryint3
+{
+     static void decimalToBinary(int num)
+    {
+        System.out.println(Integer.toBinaryString(num));
+    }
+    public static void main(String[] args)
+    {
+        int num = 5;
+ 
+        decimalToBinary(num);
+    }
+}

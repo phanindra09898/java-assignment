d8728d32d56228a04513ce38bdfa9c4e2a9ff0e0
phanindra09898 <130451254+phanindra09898@users.noreply.github.com>
2023-08-30 14:21:02 +0530

    Create gcd.java

---
+++ gcd.java
@@ -0,0 +1,13 @@
+public class GCDNumbers   
+{  
+public static void main(String[] args)   
+{  
+int x = 10, y = 11, gcd = 1;  
+for(int i = 1; i <= x && i <= y; i++)  
+{  
+if(x%i==0 && y%i==0)  
+gcd = i;  
+}  
+System.out.printf("GCD of %d and %d is: %d", x, y, gcd);  
+}  
+}  

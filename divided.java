c3ea5693895a72a2e51de5e89e17057e2b35c423
phanindra09898 <130451254+phanindra09898@users.noreply.github.com>
2023-08-30 14:35:29 +0530

    Create divided.java

---
+++ divided.java
@@ -0,0 +1,17 @@
+class divided
+{
+static void result(int N)
+    { 
+       for (int num = 0; num < N; num++)
+        {    
+            if (num % 3 == 0 && num % 5 == 0)
+                System.out.print(num + " ");
+        }
+    }
+    public static void main(String []args)
+    {
+        int N = 100;
+          
+        result(N);
+    }
+}

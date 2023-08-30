835ccdf45f479e4b147a8c8fc15259700b8ef6e8
phanindra09898 <130451254+phanindra09898@users.noreply.github.com>
2023-08-30 14:27:55 +0530

    Create simple intrest.java

---
+++ simple intrest.java
@@ -0,0 +1,12 @@
+ public class SimpleInterestCompoundInterest
+ {  
+   public static void main (String args[])  
+    {   float p, r, t, si, co;
+              p = 13000;  r = 12; t = 2;  
+               si  = (p*r*t)/100;   
+              System.out.println("Simple Interest is: " +si);  
+              co = (p*r*t)/100;
+              System.out.println("Compound Interest is: " +co);
+    }
+     
+ }  

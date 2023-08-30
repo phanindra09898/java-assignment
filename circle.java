de189c34ab4051c5cbee2450cf9e5a118263629d
phanindra09898 <130451254+phanindra09898@users.noreply.github.com>
2023-08-30 14:17:27 +0530

    Create circle.java

---
+++ circle.java
@@ -0,0 +1,28 @@
+public class Circle 
+{
+  private double radius;
+
+  public Circle(double radius) 
+{
+    this.radius = radius;
+  }
+
+  public double getArea() 
+{
+    return Math.PI * radius * radius;
+  }
+
+  public double getPerimeter() 
+{
+    return 2 * Math.PI * radius;
+}
+return 2 * (length + width);
+
+System.out.println("\nCircle: Radius-5");
+
+System.out.println("Area: " + Circle.getArea());
+
+System.out.println("Perimeter: " + Circle.getPerimeter());
+ 
+}
+    

4dd82204796a50fbab21112550cda88228c6d488
phanindra09898 <130451254+phanindra09898@users.noreply.github.com>
2023-08-30 14:37:16 +0530

    Create skipnumbers.java

---
+++ skipnumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class skipnumbers
+{
+public static void main(String[] args);
+{
+int s=30,r=80,k,i;
+
+for(i=s;i<=r;i++)
+{
+if(i%5==0)
+{
+System.out.print(" ");
+}
+else
+{
+System.out.print(" "+i);
+}
+System.out.print("\n");
+}
+}
+}
